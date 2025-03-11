50b5a05a49851987420ccba9a42edb9bef2f4c92
vertX-dev <danasolovej8@gmail.com>
2025-03-11 22:03:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables
 let selectedImageSrc = null;
 let highlightedCell = null;
-const totalRows = 10, totalCols = 10;
+const totalRows = 21, totalCols = 25;
 const gridContainer = document.getElementById('grid-container');
 
 // Create grid cells (each cell represents one “unit” of 16x16 pixels)
